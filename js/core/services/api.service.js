6b333eec7eed6dcfec870625e7925095d5976f8a
Christiaan Scheermeijer <christiaan@videodock.com>
2016-11-14 12:32:16 +0100

    Fix search error when playlist is not returned

--- js/core/services/api.service.js
+++ js/core/services/api.service.js
@@ -157,6 +157,11 @@
 
                 var feed = response.data;
 
+                // the search feed can return an empty playlist, so we can show "No results for ..."
+                if (feed && 'SEARCH' === feed.kind) {
+                    feed.playlist = feed.playlist || [];
+                }
+
                 if (!feed || !angular.isArray(feed.playlist)) {
                     return getFeedFailed(response);
                 }
