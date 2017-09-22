afae084ea190b10cabab9e7eaefcbc5ae3ad092f
Pieter <pieter.janssen.00@gmail.com>
2017-09-22 11:46:14 +0200

    Add requestAnimationFrame polyfill

--- js/polyfills.js
+++ js/polyfills.js
@@ -351,3 +351,11 @@ if ("document" in self) {
         window.addEventListener("test", null, opts);
     } catch (e) {}
 }());
+
+(function() {
+    window.requestAnimationFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) { return window.setTimeout(callback, 1000 / 60); };
+}());
